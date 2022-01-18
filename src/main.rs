95d198e091ba56e349c8afa66997d8576708eaad
prasanth <kesavarapu.siva@gmail.com>
2022-01-18 00:25:52 +0000

    start port, end port, parallize connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use clap::Parser;
 use ipnet::{IpNet, Ipv4Net};
 use rayon::prelude::*;
@@ -10,27 +11,39 @@ use std::time::Duration;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// max port to check
-    #[clap(short, long)]
-    port: u16,
+    /// start port
+    #[clap(short, long, default_value_t = 1)]
+    start: u16,
+
+    /// end port
+    #[clap(short, long, default_value_t = 1024)]
+    end: u16,
 
     /// subnet to look into
     #[clap(short, long)]
     ip: String,
+
+    /// connection timeout
+    #[clap(short, long, default_value_t = 10)]
+    timeout: u64,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
-    let net4 = IpNet::from(Ipv4Net::from_str(&args.ip).unwrap());
-    let port = args.port;
-    let t = net4.subnets(32).unwrap();
-    t.par_bridge().for_each(|ip| {
-        (1..port + 1).for_each(|x| {
-            if TcpStream::connect_timeout(&SocketAddr::new(ip.addr(), x), Duration::from_millis(10))
-                .is_ok()
+    let ipnet = IpNet::from(Ipv4Net::from_str(&args.ip)?);
+    ipnet
+        .subnets(ipnet.max_prefix_len())?
+        .flat_map(|x| (args.start..=args.end).map(move |port| (x, port)))
+        .par_bridge()
+        .for_each(|(ip, port)| {
+            if TcpStream::connect_timeout(
+                &SocketAddr::new(ip.addr(), port),
+                Duration::from_millis(args.timeout),
+            )
+            .is_ok()
             {
-                println!("ip={:?} port={x}", ip.addr(), x = x);
+                println!("success ip={:?} port={x}", ip.addr(), x = port);
             }
-        })
-    });
+        });
+    Ok(())
 }
